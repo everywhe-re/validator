4f78a3a0160b87d250912c7fbd8f210a1533ba5d
leNicDev <germanelectronix@gmail.com>
2018-05-17 22:51:09 +0200

    Fixes

---
+++ config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    /**
+     * IP Addresses for each validator node
+     */
+    peers: [
+        '140.82.0.158', // Jason
+        '149.28.108.86'  // Darcy
+    ]
+}
--- src/app.js
+++ src/app.js
@@ -1,26 +1,37 @@
 const lotion = require('lotion');
 
 const genesis = require.resolve('../genesis.json');
-const validators = require('../config/validators');
+const lotionPort = 3000;
+const config = require('../config/config');
+const dev = process.env.DEV || false;
 
-// Configure lotion
+// Build basic configuration
 const options = {
-    lotionPort: 3000,
+    lotionPort: lotionPort,
     p2pPort: 46656,
     tendermintPort: 46657,
-    peers: validators.map((addr) => `${addr}:46656`),
-    genesis: genesis,
     initialState: {
         messages: [{ sender: 'Nic', message: 'Roses are red, violets are blue and centralization is an old shoe.' }]
     }
 };
 
+// Update config based on the environment
+if (dev) {
+    options.devMode = true;
+} else {
+    options.keys = 'validator_priv.json';
+    options.peers = config.peers.map((addr) => `${addr}:46656`);
+    options.genesis = genesis;
+}
+
+// Print config
 console.log('Using config', options);
 
+// Initialize lotion app
 let app = lotion(options);
 
 // Middleware MUST be deterministic!
-function msgHandler(state, tx) {
+const msgHandler = (state, tx) => {
     // tx.sender has wrong type
     if (typeof tx.sender !== 'string') {
         return;
@@ -73,8 +84,7 @@ app.use(msgHandler);
 app.listen(3000).then(genesis => {
     console.log(`App listening on port ${options.lotionPort}.`);
     console.log(genesis);
-})
-.catch(err => {
+}, (err) => {
     console.error('Failed to start app', err);
 });
 
